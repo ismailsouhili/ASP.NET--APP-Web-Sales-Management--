d7c1570a8405ccf87d8c28dc888b1a7f97672694
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2021-10-18 16:44:09 +0000

    Create Projects.aspx.cs

---
+++ AppWeb-Sales Management/Projects.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AppWeb_Sales_Management
+{
+    public partial class Projects : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Userid"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            LibraryDb.SelectCommand = "SELECT [ProjId],[ProjectName],dbo.fn_getUserName([ProjectManager]) as [ProjectManager],CONVERT(CHAR(23),CONVERT(DATETIME,timestamp,101),121) as timestamp,[Amount],[Revenue],[Description],[status],CONVERT(CHAR(23),CONVERT(DATETIME,LaunchDate,101),121) as [LaunchDate],[Duration],[ContactName],[ContactMobile],[Notes] FROM [dbo].[Project_Table]  Where [ProjectManager]='" + Session["UserId"].ToString() + "' ORDER BY[ProjectName]";
+
+        }
+
+        protected void LibraryDb_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+    }
+}
